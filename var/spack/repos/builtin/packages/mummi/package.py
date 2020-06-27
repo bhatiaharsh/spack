042aa73f8976c029be19250b5b66e88a667a40f9
Harsh Bhatia <bhatia4@llnl.gov>
2020-06-27 11:52:08 -0700

    updates to mummi package

--- var/spack/repos/builtin/packages/mummi/package.py
+++ var/spack/repos/builtin/packages/mummi/package.py
@@ -35,8 +35,19 @@ class Mummi(PythonPackage):
 
         version('develop', branch='develop')
 
-    # -------------------------------------------------------------------
+        variant('cudaml', default=False, description='Build ML framework with Cuda')
 
+
+    '''
+    ## To install, pass mpi, cuda, etc. explicitly
+
+    lassen:
+        spack install mummi@develop +cudaml ^cudnn@7.6.5.32-10.2-ppc64le ^cuda@10.1.243 ^spectrum-mpi@2019.06.24 %gcc@7.3.1
+        spack install mummi@develop +cudaml ^cudnn@7.6.5.32-10.2-ppc64le ^cuda@10.2.89  ^spectrum-mpi@2019.06.24 %gcc@7.3.1
+        spack install mummi@develop                                      ^cuda@10.1.243 ^spectrum-mpi@2019.06.24 %gcc@7.3.1
+        spack install mummi@develop                                      ^cuda@10.2.89  ^spectrum-mpi@2019.06.24 %gcc@7.3.1
+    '''
+    # -------------------------------------------------------------------
     extends('python@3.7.6')
 
     # build dependencies
@@ -44,22 +55,27 @@ class Mummi(PythonPackage):
     depends_on('swig@3.0.12',  type='build')
 
     # generic
-    depends_on('py-numpy@1.16.4')
-    depends_on('py-scipy@1.3.0')
+    depends_on('py-numpy') #@1.16.4')
+    depends_on('py-scipy') #@1.3.0')
     
     # ml
-    depends_on('cudnn@7.5.1-10.1-ppc64le')			      	        #TODO: these settings are for powerpc
+    depends_on('py-h5py +mpi')
     depends_on('faiss@1.5.3 +python')
-    #depends_on('py-theano@1.0.4 +cuda ^cudnn@7.5.1-10.1-ppc64le')	#TODO: these settings are for powerpc
-    #depends_on('py-theano@1.0.4 +cuda')
+
+    
+    # these settings are for powerpc (actually, only cudann only for lassen)
+    #depends_on('cudnn@7.6.5.32-10.2-ppc64le', when='+cudaml')
+    #depends_on('cudnn@7.5.1-10.1-ppc64le', when='+cudaml')
+    #depends_on('py-theano@1.0.4 +cuda ^cudnn@7.5.1-10.1-ppc64le', when='+cudaml')
+
+    depends_on('py-theano +cuda', when='+cudaml')
     depends_on('py-keras@2.2.4')
-    #depends_on('py-h5py@2.9.0~mpi ^hdf5~mpi+hl')
 
     # analysis
     depends_on('talass@process-statistics')
     depends_on('py-mdanalysis-mummi@mda0.19.2_ddcmd_lessmem')
     depends_on('py-scikit-learn')
-    depends_on('py-matplotlib@3.0.2')
+    depends_on('py-matplotlib') #@3.0.2')
 
     # macro
     depends_on('gridsim2d')
