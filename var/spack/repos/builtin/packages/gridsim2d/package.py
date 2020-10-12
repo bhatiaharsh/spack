f93f9513f7d70792e935f05470d24f7da143ef7a
Harsh Bhatia <bhatia4@llnl.gov>
2020-10-12 10:30:00 -0700

    added parmed

--- var/spack/repos/builtin/packages/gridsim2d/package.py
+++ var/spack/repos/builtin/packages/gridsim2d/package.py
@@ -5,13 +5,27 @@
 
 from spack import *
 
+import os
 
 class Gridsim2d(MakefilePackage):
 
     git        = "ssh://git@cz-bitbucket.llnl.gov:7999/~tomaso/gridcorr2d.git"
     homepage   = "https://lc.llnl.gov/bitbucket/users/tomaso/repos/gridcorr2d/browse"
+
     version('master', branch='master')
+    version('campaign-3', branch='Campaign-3')
+    version('v2020-09-16', tag='v2020-09-16')
+    version('v2020-10-01', tag='v2020-10-01')
+
+    depends_on('mpi')
+    depends_on('fftw')
 
     def install(self, spec, prefix):
         mkdir(prefix.bin)
         install('gridsim2dras', prefix.bin)
+
+        with working_dir('c3-test'):
+            mfiles = ['pmfcode.m', 'make_pmf.m', 'interpolate.m', 'pmfsmooth2.m']
+            for m in mfiles:
+                install(m, prefix.bin)
+
