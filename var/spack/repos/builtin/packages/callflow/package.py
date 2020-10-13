0826b918f9acd5fbb4abeb5a4ecce56712d65d74
Harsh Bhatia <bhatia4@llnl.gov>
2020-10-12 23:11:07 -0700

    added client logiic

--- var/spack/repos/builtin/packages/callflow/package.py
+++ var/spack/repos/builtin/packages/callflow/package.py
@@ -3,6 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+import stat
+import shutil
 from spack import *
 
 
@@ -17,6 +20,7 @@ class Callflow(PythonPackage):
     maintainers = ["bhatiaharsh", "jarusified"]
 
     version('1.1.0', sha256='f8b875eb62fbac04b117e3c23fccff99d768158226a9b7fa222a2b2a6acafa44')
+    version('test', git='git@github.com:jarusified/CallFlow.git', branch='fix/installation')
 
     depends_on('python@3.6:',       type=('build', 'run'))
     depends_on('py-setuptools',     type='build')
@@ -38,3 +42,23 @@ class Callflow(PythonPackage):
     depends_on('py-flask',          type=('build', 'run'))
     depends_on('py-flask-socketio', type=('build', 'run'))
     depends_on('node-js@13.8: ~without-npm', type=('build', 'run'))
+
+    # Compile the npm modules included in the project
+    @run_after('install')
+    def build_app(self):
+
+        src = os.path.join(os.getcwd(), 'app')
+        dst = os.path.join(self.spec.prefix, 'app')
+        shutil.copytree(src, dst)
+
+        with working_dir(dst):
+            os.system('npm install .')
+
+        fname = os.path.join(self.spec.prefix.bin, 'callflow_client')
+        with open(fname, 'w') as f:
+            f.write('#!/bin/sh\n')
+            f.write('cd {}\n'.format(dst))
+            f.write('npm run dev\n')
+
+        st = os.stat(fname)
+        os.chmod(fname, st.st_mode | stat.S_IEXEC)
