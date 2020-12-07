d97213caeefebdcd6a3cae61214ee21108e96290
Harsh Bhatia <bhatia4@llnl.gov>
2020-12-07 10:30:21 -0800

    more changes

--- var/spack/repos/builtin/packages/gridsim2d/package.py
+++ var/spack/repos/builtin/packages/gridsim2d/package.py
@@ -21,6 +21,7 @@ class Gridsim2d(MakefilePackage):
     version('v2020-10-16-summit', tag='v2020-10-16-summit')
     version('v2020-10-23-scaling', tag='v2020-10-23-scaling')  ## Added by tomaso, 2020-10-23
     version('v2020-11-13', tag='v2020-11-13')  ## Added by tomaso, 2020-11-13
+    version('v2020-12-04', tag='v2020-12-04')
 
     depends_on('mpi')
     depends_on('fftw')
@@ -38,7 +39,7 @@ class Gridsim2d(MakefilePackage):
                       'pmf_mRASa_smooth.txt','pmf_mRASb_smooth.txt','pmf_zRAFa_smooth.txt', \
                       'pmf_mRAFa_smooth.txt','pmf_mRAFb_smooth.txt','pmf_zRASa_smooth.txt', \
                       'pmfcode.m','make_pmf.m','pmfsmooth2.m','interpolate.m']
-
+            mfiles = [] # This is for version v2020-12-04
             for m in mfiles:
                 install(m, prefix.bin)
 
