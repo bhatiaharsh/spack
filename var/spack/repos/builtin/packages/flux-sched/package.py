1bc64c10fecc0c3b6041a660759172b44669b2e5
Harsh Bhatia <bhatia4@llnl.gov>
2020-12-15 12:20:55 -0800

    squash and merge "bhatia-mummi2" branch (copied into mummi2)
    * updates to existing packages for mummi
    
    * new packages for mummi
    
    * added some notes to mummi
    
    * udpated maestro's spec
    
    * add python3 mummi, maestrowf
    
    * updated memsurfer
    
    * add gridcorr2d package
    
    * Revert "add gridcorr2d package"
    
    This reverts commit 02d457ea047ab8c2a9e66e6df93e94f771c275d9.
    
    * Revert "add python3 mummi, maestrowf"
    
    This reverts commit ba8e33ce0764cb8e755af18564741ec62dfaa7db.
    
    * Add python3 branch to mummi
    
    * add gridcorr2d
    
    * updates to maestro
    
    * rename to mummi-macro
    
    * add h5py dependency to mummi
    
    * joseph's updates to flux
    
    * update dependent packages
    
    * remove theano to prevent stalling installation
    
    * included new ddcmdconverter
    
    * add pytaridx
    
    * changes to gromacs
    
    * added ddcmdconverter
    
    * consistncy with mummi
    
    * updates to mummi package
    
    * updated faiss, but wont work for cuda
    
    * faiss package
    
    * working faiss
    
    * new faiss
    
    * updated ddcmdconverter
    
    * fix gromacs->fftw
    
    * fixes to dbr
    
    * fixes to many
    
    Conflicts:
    	var/spack/repos/builtin/packages/flux-core/package.py
    
    * updates to dbr
    
    * updates
    
    Conflicts:
    	var/spack/repos/builtin/packages/py-jsonschema/package.py
    	var/spack/repos/builtin/packages/py-pyrsistent/package.py
    
    * databroker udate
    
    * udpate to mdanalysis
    
    * added parmed
    
    * wip
    
    Conflicts:
    	var/spack/repos/builtin/packages/mummi/package.py
    
    * move to summiit
    
    * fixed chksum
    
    * updates to databroker
    
    * more fixes on summit
    
    Co-authored-by: Joseph Moon <moon15@llnl.gov>
    Co-authored-by: Joseph Moon <jmoon1506@gmail.com>
    Co-authored-by: Joseph Moon <moon15@login1.summit.olcf.ornl.gov>
    Co-authored-by: Splash User <splash@sierra4359.coral.llnl.gov>

--- var/spack/repos/builtin/packages/flux-sched/package.py
+++ var/spack/repos/builtin/packages/flux-sched/package.py
@@ -46,6 +46,7 @@ class FluxSched(AutotoolsPackage):
     depends_on("flux-core@0.11.0", when='@0.7.0', type=('build', 'run', 'link'))
     depends_on("flux-core@0.11.2:0.11.99", when='@0.7.1', type=('build', 'run', 'link'))
     depends_on("flux-core@0.16.0:0.16.99", when='@0.8.0', type=('build', 'run', 'link'))
+    depends_on("flux-core@0.19.0", when='@0.11.0', type=('build', 'run', 'link'))
     depends_on("flux-core@master", when='@master', type=('build', 'run', 'link'))
 
     # Need autotools when building on master:
