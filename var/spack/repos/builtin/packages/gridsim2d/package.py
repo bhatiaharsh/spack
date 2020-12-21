1457cedfe050d314ad03c0e099f8f4c394ee0f43
Harsh Bhatia <bhatia4@llnl.gov>
2020-12-21 11:24:06 -0800

    updated to maestro and gridsim

--- var/spack/repos/builtin/packages/gridsim2d/package.py
+++ var/spack/repos/builtin/packages/gridsim2d/package.py
@@ -19,9 +19,10 @@ class Gridsim2d(MakefilePackage):
     version('v2020-10-09', tag='v2020-10-09')
     version('v2020-10-09.2', tag='v2020-10-09.2')
     version('v2020-10-16-summit', tag='v2020-10-16-summit')
-    version('v2020-10-23-scaling', tag='v2020-10-23-scaling')  ## Added by tomaso, 2020-10-23
-    version('v2020-11-13', tag='v2020-11-13')  ## Added by tomaso, 2020-11-13
+    version('v2020-10-23-scaling', tag='v2020-10-23-scaling')   ## Added by tomaso, 2020-10-23
+    version('v2020-11-13', tag='v2020-11-13')                   ## Added by tomaso, 2020-11-13
     version('v2020-12-04', tag='v2020-12-04')
+    version('v2020-12-21-c3-final', tag='v2020-12-21-c3-final') ## Added by tomaso, 2020-12-21
 
     depends_on('mpi')
     depends_on('fftw')
@@ -30,16 +31,23 @@ class Gridsim2d(MakefilePackage):
         mkdir(prefix.bin)
         install('gridsim2dras', prefix.bin)
 
-        with working_dir('c3-test'):
-            #mfiles = ['pmfcode.m', 'make_pmf.m', 'interpolate.m', 'pmfsmooth2.m']
-            ## Added by tomaso 2020-11-13:
-            mfiles = ['run2400hack.cfg','atoms.init','bonds.init', \
-                      'cofr_ij_hack_smooth_full.dat','cofk_ij.txt','rep12_pot.dat', \
-                      'pmf_RASa_smooth.txt' ,'pmf_RASb_smooth.txt' ,'pmf_RASc_smooth.txt' , \
-                      'pmf_mRASa_smooth.txt','pmf_mRASb_smooth.txt','pmf_zRAFa_smooth.txt', \
-                      'pmf_mRAFa_smooth.txt','pmf_mRAFb_smooth.txt','pmf_zRASa_smooth.txt', \
-                      'pmfcode.m','make_pmf.m','pmfsmooth2.m','interpolate.m']
-            mfiles = [] # This is for version v2020-12-04
+        '''
+        with working_dir('c3-final'):
+            ## mfiles desginate files needed for running and for starting a simulation from scratch
+            mfiles = [ 'run-1um.cfg', \ ## Main input file
+                       'make_pmf.m','pmfcode.m','interpolate.m','pmfsmooth2.m', \ ## m-files for feedback
+                       'cofk_ij_hack-g2.txt','cofr_ij_hack_smooth-g2.dat', \ ## Correlation functions
+                       'atoms.init','bonds.init', \ ## Initial conditions for proteins
+                       'pmf_mRAFb-g2.txt','pmf_zero.txt'    , \ ## Remaining files are potentials
+                       'pmf_RASa-g2.txt' ,'pmf_mRASa-g2.txt', \
+                       'pmf_RASb-g2.txt' ,'pmf_mRASb-g2.txt', \
+                       'pmf_RASc-g2.txt' ,'pmf_zRAFa-g2.txt', \
+                       'pmf_mRAFa-g2.txt','pmf_zRASa-g2.txt', \
+                       'rep12_pot.dat' \ ## Protein-protein repulsive potential
+            ]
+
+
+            mfiles = [] # Set this if no files need copying, e.g. continuing existin simulation
             for m in mfiles:
                 install(m, prefix.bin)
-
+        '''
--- var/spack/repos/builtin/packages/py-maestrowf/package.py
+++ var/spack/repos/builtin/packages/py-maestrowf/package.py
@@ -22,6 +22,8 @@ class PyMaestrowf(PythonPackage):
     version('flux-dev', branch='bugfix/flux_broker')
     version('flux-c3', branch='merge/flux2')
     version('flux-c3.beta_1123', tag='flux_beta_1123')
+    version('flux_beta_122120', tag='flux_beta_122120')
+
 
     # Pre-release candidates
     version('1.1.5dev',    sha256='eb3d6f31c233e2cde3b84e15c657002b83ff43d4d6b218b33d023a4f527b9e08')
